b47f0bc2df862870adb60718ff3665f558835d1f
unknown <NadaHafiz>
2022-08-14 16:32:21 +0300

    Add \n twice

--- Lab 1/Lab1.c
+++ Lab 1/Lab1.c
@@ -1,5 +1,5 @@
 
 #include <stdio.h>
 void main (){
-printf("My name is Nada \nMy birthday is 12 Nov 2002 \nStudying Biomedical Engineering");
+printf("My name is Nada \n\nMy birthday is 12 Nov 2002 \n\nStudying Biomedical Engineering");
 }
