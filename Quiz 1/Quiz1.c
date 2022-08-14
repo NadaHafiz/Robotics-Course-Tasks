d3bc3cf5bf4ac1f5299b6979e96fa455a63ecaaf
unknown <NadaHafiz>
2022-08-14 22:26:01 +0300

    Quiz 1

---
+++ Quiz 1/Quiz1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+int number1,number2;
+
+printf("Please Enter The Two Number You Want To Find The Maximum");
+scanf("%d%d",&number1,number2);
+
+switch(number1>number2)
+{
+case 0:
+printf("The Maximum Number Is %d",number2);
+break;
+
+case 1:
+printf("The Maximum Number Is %d",number1);
+break;
+}
+}
