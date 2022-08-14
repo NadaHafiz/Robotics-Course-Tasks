9400283ce0d0d220c0fa86acf78dc4c274bd3c7d
unknown <NadaHafiz>
2022-08-14 22:43:34 +0300

    Quiz 2

---
+++ Quiz 2/Quiz2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int num,i;
+
+    printf("Enter Starting Value: ");
+    scanf("%d",&num);
+
+    for(i=num;i>0;--i)
+	{
+     printf("%d \n",i);
+    }
+	return 0;
+}
