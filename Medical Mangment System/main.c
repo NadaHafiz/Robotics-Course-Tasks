c0a2dc5cb97edafd9e0f043722a363038dc3004c
unknown <NadaHafiz>
2022-08-28 23:58:37 +0300

    Editing Cancel Reservation Function

--- Medical Mangment System/main.c
+++ Medical Mangment System/main.c
@@ -290,13 +290,23 @@ void cancel_reservation()
             {
                 while(id==array_id[i])
                 {
+					if(0==array_res[i])
+					{
+						printf(" There is No Reservation Yet\n\n");
+						id_check=1;
+						break;
+					}
+					else
+					{
                     printf(" Cancelling You Reservation..\n");
+					printf(" Reservation Cancelled Successfully\n\n");
                     array_res[i]=0;
                     id_check=1;
                     break;
+					}
+					break;
                 }
             }
-        printf(" Reservation Cancelled Successfully\n\n");
         if(id_check==0)
             {
                 printf(" ID Not Found\n\n");
